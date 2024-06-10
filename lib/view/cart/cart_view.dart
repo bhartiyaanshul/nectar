d358de9eade8bb07418ab9dec4dc71729f84b1c5
Anshul Bhartiya <126051112+bhartiyaanshul@users.noreply.github.com>
2024-06-10 15:10:46 +0530

    private provider added in cart view

--- lib/view/cart/cart_view.dart
+++ lib/view/cart/cart_view.dart
@@ -10,44 +10,52 @@ class CartView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final model = context.watch<CartViewModel>();
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Cart'),
-        ),
-        body: Center(
-          child: Column(
-            children: [
-              ListView.builder(
-                shrinkWrap: true,
-                itemCount: model.items.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(model.items[index].name),
-                    trailing: IconButton(
-                      icon: const Icon(Icons.remove),
-                      onPressed: () {
-                        model.remove(model.items[index]);
+    // final model = context.watch<CartViewModel>();
+    return ChangeNotifierProvider(
+      create:(_) => CartViewModel(),
+      child: Builder(
+        builder: (context) {
+          final model = context.watch<CartViewModel>();
+          return Scaffold(
+              appBar: AppBar(
+                title: const Text('Cart'),
+              ),
+              body: Center(
+                child: Column(
+                  children: [
+                    ListView.builder(
+                      shrinkWrap: true,
+                      itemCount: model.items.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text(model.items[index].name),
+                          trailing: IconButton(
+                            icon: const Icon(Icons.remove),
+                            onPressed: () {
+                              model.remove(model.items[index]);
+                            },
+                          ),
+                        );
                       },
                     ),
-                  );
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () {
+                        model.removeAll();
+                      }, 
+                      child: const Text('Remove All')
+                    ),
+                  ],
+                ),
+              ),
+              floatingActionButton: FloatingActionButton(
+                onPressed: (){
+                  model.add(Item(name: 'Item ${model.items.length+1}'));
                 },
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  model.removeAll();
-                }, 
-                child: const Text('Remove All')
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: (){
-            model.add(Item(name: 'Item ${model.items.length+1}'));
-          },
-          child: const Icon(Icons.add),
-        ));
+                child: const Icon(Icons.add),
+              ));
+        }
+      ),
+    );
   }
 }
