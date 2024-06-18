8a97bcdb46cd82b2efb53eb13ebbce24ffea2cfa
Anshul Bhartiya <126051112+bhartiyaanshul@users.noreply.github.com>
2024-06-18 17:27:47 +0530

    Home Screen UI

--- lib/view/home_view/home_view.dart
+++ lib/view/home_view/home_view.dart
@@ -1,11 +1,14 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:nectar/core/app_locator.dart';
 import 'package:nectar/core/app_router.gr.dart';
 import 'package:nectar/core/service/auth_service.dart';
+import 'package:nectar/widget/cards/product_card.dart';
 import 'package:nectar/widget/searchfield/primary_searchfield.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 @RoutePage()
 class HomevView extends StatelessWidget {
@@ -13,52 +16,194 @@ class HomevView extends StatelessWidget {
 
   final _auth = locator<AuthService>();
 
+  final controller = PageController();
+  int length = 3;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      appBar: AppBar(
-        // leading: IconButton(
-        //   icon: const Icon(Icons.arrow_back_ios),
-        //   onPressed: () {
-        //     // context.router.push(const CartRoute());
-        //   },
-        // ),
-        // actions: [
-        //   IconButton(
-        //     icon: const Icon(Icons.logout),
-        //     onPressed: () async {
-        //       await _auth.signOut();
-        //       context.router.replace(const OnBoardingRoute());
-        //     },
-        //   ),
-        // ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(25.0),
-        child: Center(
-          child: Column(
-            children: [
-              SvgPicture.asset('assets/icons/logo-vector.svg'),
-              const SizedBox(height: 8),
-              const Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
+      body: SafeArea(
+        child: ListView(
+          children: [
+            SvgPicture.asset('assets/icons/logo-vector.svg'),
+            const SizedBox(height: 8),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.location_on,
+                  size: 30,
+                  color: Color(0xff4C4F4D),
+                ),
+                SizedBox(width: 8),
+                Text(
+                  'Dhaka, Banassree',
+                  style: TextStyle(
+                      fontSize: 18,
+                      color: Color(0xff4C4F4D),
+                      fontWeight: FontWeight.w500,
+                      fontFamily: 'Gilroy'),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: PrimarySearchField(
+                hintText: 'Search Store',
+                prefixicon: SvgPicture.asset(
+                  'assets/icons/search.svg',
+                  width: 20,
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              height: 115,
+              child: Stack(
                 children: [
-                  Icon(Icons.location_on, size: 30,color: Color(0xff4C4F4D),),
-                  SizedBox(width: 8),
-                  Text('Dhaka, Banassree', style: TextStyle(fontSize: 18, color: Color(0xff4C4F4D),fontWeight: FontWeight.w500, fontFamily: 'Gilroy'),),
+                  PageView.builder(
+                      controller: controller,
+                      itemCount: 3,
+                      itemBuilder: (_, i) {
+                        return Image.asset('assets/images/banner.png');
+                      }),
+                  Positioned(
+                    bottom: 15,
+                    left: 150,
+                    child: SmoothPageIndicator(
+                      controller: controller,
+                      count: length,
+                      effect: const ExpandingDotsEffect(
+                        dotWidth: 10,
+                        dotHeight: 10,
+                        activeDotColor: Color.fromARGB(255, 45, 144, 58),
+                        dotColor: Color(0xff919191),
+                      ),
+                    ),
+                  ),
                 ],
               ),
-              const SizedBox(height: 20),
-              PrimarySearchField(
-                hintText: 'Search Store',
-                prefixicon: SvgPicture.asset('assets/icons/search.svg',width: 20,),
+            ),
+            const SizedBox(height: 30),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Row(
+                children: [
+                  const Text('Exclusive Offer',
+                      style: TextStyle(fontSize: 24, fontFamily: 'Gilroy')),
+                  const Spacer(),
+                  InkWell(
+                    child: TextButton(
+                      child: const Text('See all',
+                          style: TextStyle(
+                              fontFamily: 'gilroy',
+                              fontSize: 16,
+                              color: Color(0xff53B175))),
+                      onPressed: () {},
+                    ),
+                  )
+                ],
               ),
-              const SizedBox(height: 20),
-            ],
-          ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              height: 250,
+              child: ListView.builder(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                itemCount: 5,
+                itemBuilder: (_, i) {
+                  return const Row(
+                    children: [
+                      ProductCard(
+                          imageUrl: 'assets/icons/logo.png',
+                          productName: 'name',
+                          productDesc: 'name',
+                          price: '500'),
+                      SizedBox(width: 15),
+                    ],
+                  );
+                },
+              ),
+            ),
+            const SizedBox(height: 30),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25),
+              child: Row(
+                children: [
+                  const Text('Best Selling',
+                      style: TextStyle(fontSize: 24, fontFamily: 'Gilroy')),
+                  const Spacer(),
+                  InkWell(
+                    child: TextButton(
+                      child: const Text('See all',
+                          style: TextStyle(
+                              fontFamily: 'gilroy',
+                              fontSize: 16,
+                              color: Color(0xff53B175))),
+                      onPressed: () {},
+                    ),
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              height: 250,
+              child: ListView.builder(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                itemCount: 5,
+                itemBuilder: (_, i) {
+                  return const Row(
+                    children: [
+                      ProductCard(
+                          imageUrl: 'assets/icons/logo.png',
+                          productName: 'name',
+                          productDesc: 'name',
+                          price: '500'),
+                      SizedBox(width: 15),
+                    ],
+                  );
+                },
+              ),
+            ),
+          ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        // fixedColor: Colors.black,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.storefront_outlined),
+            label: 'Shop',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.travel_explore_outlined),
+            label: 'Explore',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart_outlined),
+            label: 'Cart',
+          ),
+          // BottomNavigationBarItem(
+          //   icon: Icon(Icons.favorite_border_outlined),
+          //   label: 'Favourite',
+          // ),
+          // BottomNavigationBarItem(
+          //   icon: Icon(Icons.person_outline),
+          //   label: 'Account',
+          // ),
+        ],
       ),
     );
   }
---
+++ lib/widget/cards/product_card.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class ProductCard extends StatelessWidget {
+  final String? imageUrl;
+  final String? productName;
+  final String? productDesc;
+  final String? price;
+  const ProductCard({super.key,required this.imageUrl, required this.productName, required this.productDesc, required this.price});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        border: Border.all(
+          color: const Color(0xffE2E2E2)
+        ),
+      ),
+      width: 170,
+      height: 250,
+      padding: const EdgeInsets.all(15),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(child: Image.asset(imageUrl!, width: 100,height: 80)),
+          const SizedBox(height: 25),
+          Text(productName!, style: const TextStyle(fontFamily: 'gilroy', fontSize: 16, color: Color(0xff181725)),),
+          const SizedBox(height: 5),
+          Text(productDesc!, style: const TextStyle(fontFamily: 'gilroy', fontSize: 14, color: Color(0xff7C7C7C)),),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("\$ $price", style: const TextStyle(fontFamily: 'gilroy', fontSize: 16, color: Color(0xff181725)),),
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                decoration: BoxDecoration(
+                  color: const Color(0xff53B175),
+                  borderRadius: BorderRadius.circular(17)
+                ),
+                child: const Icon(Icons.add, color: Colors.white,),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
